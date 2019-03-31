590553ee460ccbb56c80f2fb042e35a0a0502701
Gabriel Rodrigues <grfgabriel.ti@gmail.com>
2019-03-30 21:47:01 -0300

    ajust for locate face

--- analyze_face_image.py
+++ analyze_face_image.py
@@ -1,14 +1,11 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_eye.xml')
 
 image = cv2.imread("images/people.jpg")
 
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(image_gray, scaleFactor=1.04, minNeighbors=9)
-faces_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-eyes = eye_cascade.detectMultiScale()
 for (x,y,w,h) in faces:
 	cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2)
 
